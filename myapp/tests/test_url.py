491a078149f8819889a5b824c30f7745d92e180d
MaximBall <maks1111bm@gmail.com>
2021-09-18 15:47:31 +0300

    created a test to check the HTTP code for url '/ sales'

--- myapp/tests/test_url.py
+++ myapp/tests/test_url.py
@@ -3,14 +3,18 @@ from django.test import TestCase
 
 class StatusCodeTests(TestCase):
     
-    def test_main_url(self):
+   def test_main_url(self):
        response = self.client.get('/')
        self.assertEqual(response.status_code, 200)
 
-    def test_buy_apple_url(self):
+   def test_buy_apple_url(self):
        response = self.client.get('/buy/apple')
        self.assertEqual(response.status_code, 200)
 
-    def test_login_url(self):
+   def test_login_url(self):
        response = self.client.get('/login')
+       self.assertEqual(response.status_code, 200)
+
+   def test_sales_list_url(self):
+       response = self.client.get('/sales')
        self.assertEqual(response.status_code, 200)
