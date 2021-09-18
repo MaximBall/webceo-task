0c63e437c07a222792810a2d3a3bf73303971602
MaximBall <maks1111bm@gmail.com>
2021-09-18 12:50:49 +0300

    bootstrap is connected; improved index_html template

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import ItemListView
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', ItemListView.as_view(), name='item_list')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
