86c559459c9ea75c6580a87d38af0dd4c82e221d
MaximBall <maks1111bm@gmail.com>
2021-09-18 15:23:55 +0300

    created a test to check the HTTP code for url '/ login'

--- myapp/tests/test_url.py
+++ myapp/tests/test_url.py
@@ -10,3 +10,7 @@ class StatusCodeTests(TestCase):
     def test_buy_apple_url(self):
        response = self.client.get('/buy/apple')
        self.assertEqual(response.status_code, 200)
+
+    def test_login_url(self):
+       response = self.client.get('/login')
+       self.assertEqual(response.status_code, 200)
