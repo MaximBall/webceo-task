7aeea92554fe8182c6b4375d6fbc24873f3d52af
MaximBall <maks1111bm@gmail.com>
2021-09-18 18:52:35 +0300

    added context processor to output the date and time of page generation to the context of all application templates

--- myapp/settings.py
+++ myapp/settings.py
@@ -59,6 +59,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'myapp.context_processors.get_current_year_to_context',
             ],
         },
     },
