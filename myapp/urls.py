40e0fec381ec8a724a4c6017fde8635d2d075185
MaximBall <maks1111bm@gmail.com>
2021-09-18 13:29:30 +0300

    created a template 'buy item' and a view for it with a get method; corresponding url created

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,13 +1,15 @@
+from os import name
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import ItemListView
+from .views import ItemListView, BuyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', ItemListView.as_view(), name='item_list')
+    path('', ItemListView.as_view(), name='item_list'),
+    path('buy/<str:name>', BuyView.as_view(), name='buy_item'),
 ]
 
 if settings.DEBUG:
--- myapp/views.py
+++ myapp/views.py
@@ -9,3 +9,16 @@ class ItemListView(View):
         items = Item.objects.all()
         context = {"items": items}
         return render(request, "item_list.html", context)
+
+class BuyView(View):
+
+    def get(self, request, name):
+
+        # item = Item.objects.first(name=name)
+        items = Item.objects.filter(name=name)
+        context = {"items": items, "name": name}
+        return render(request, "buy_item.html", context)
+        
+
+    def post(self, request, name):
+        pass
