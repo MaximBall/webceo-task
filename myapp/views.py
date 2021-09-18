2d0bebff074b5b22e868f236e934265c954d2628
MaximBall <maks1111bm@gmail.com>
2021-09-18 13:54:35 +0300

    added POST method to BuyItem view to create Sale table record

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from .models import Item
+from django.contrib.auth.models import User
+from .models import Employee, Item, Sale
 from django.views.generic import View
+from django.http import HttpResponseRedirect
+import datetime
+
 
 class ItemListView(View):
     
@@ -14,11 +18,32 @@ class BuyView(View):
 
     def get(self, request, name):
 
-        # item = Item.objects.first(name=name)
         items = Item.objects.filter(name=name)
         context = {"items": items, "name": name}
         return render(request, "buy_item.html", context)
         
 
     def post(self, request, name):
-        pass
+        
+        count = int(request.POST.get("count_item"))
+        select_dropdown = request.POST.get("dropdown")
+        list_data = select_dropdown.split()
+
+        user = User.objects.get(username=list_data[0])
+        employe = Employee.objects.get(user=user)
+        item = Item.objects.get(name=name, employe=employe, price=list_data[1])
+
+        total_price = count * item.price
+
+        new_sale = Sale(
+            item=item,
+            employe=employe,
+            count=count,
+            total_price=total_price,
+            price_one_item=item.price,
+            date_sale=datetime.datetime.now()
+        )
+
+        new_sale.save()
+        route = "/"
+        return HttpResponseRedirect(route)
