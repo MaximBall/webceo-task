76e7f27b5e25d91aa282332f0948d1e8b17d198a
MaximBall <maks1111bm@gmail.com>
2021-09-30 11:38:10 +0300

    delete staticroot folder and update requirements

--- myapp/settings.py
+++ myapp/settings.py
@@ -117,11 +117,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
-
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
